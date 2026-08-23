af3180766ca045e4d3955f9adb485494ea2eee0e
aleray0saslal <t2wmt6sgff@privaterelay.appleid.com>
2026-08-23 20:07:27 +0200

    Actualizar ContestTrackerApp.swift

--- SwiftPlayground/ContestTrackerApp.swift
+++ SwiftPlayground/ContestTrackerApp.swift
@@ -3,18 +3,35 @@ import SwiftData
 
 @main
 struct ContestTrackerApp: App {
+    let sharedModelContainer: ModelContainer
+    
+    init() {
+        do {
+            let configuration = ModelConfiguration(
+                isStoredInMemoryOnly: false
+            )
+            
+            let container = try ModelContainer(
+                for:
+                    Contest.self,
+                MusicWork.self,
+                ContestPhase.self,
+                ContestRepertoireItem.self,
+                ContestArchive.self,
+                configurations: configuration
+            )
+            
+            sharedModelContainer = container
+            
+        } catch {
+            fatalError("No se pudo crear el ModelContainer: \(error)")
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
-        .modelContainer(
-            for: [
-                Contest.self,
-                MusicWork.self,
-                ContestPhase.self,
-                ContestRepertoireItem.self,
-                ContestArchive.self
-            ]
-        )
+        .modelContainer(sharedModelContainer)
     }
 }
